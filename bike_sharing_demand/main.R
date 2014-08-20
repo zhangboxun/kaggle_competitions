a34b69aee8105f15fb0e689afb68b2a5f871c062
Boxun Zhang <zhangboxun@gmail.com>
2014-08-20 11:15:59 +0800

    Initial check-in

---
+++ bike_sharing_demand/main.R
@@ -0,0 +1,135 @@
+setwd("~/workspace/kaggle_competitions/bike_sharing_demand/")
+
+library(ggplot2)
+library(lubridate)
+library(corrplot)
+library(e1071)
+library(caret)
+library(doMC)
+
+preprocess.data <- function() {
+  # laod the raw training set
+  train <- read.table("data//train.csv", header=T, sep=",")
+  
+  # convert categorical variables into factors
+  train$season <- factor(train$season)
+  train$holiday <- factor(train$holiday)
+  train$workingday <- factor(train$workingday)
+  train$weather <- factor(train$weather)
+  
+  # The count of bike rented exhibits strong daily effect,
+  # so the hour of day is extracted from datetime column as a new feature
+  # and is treated as numerical value instead of categorical value.
+  train$datetime <- ymd_hms(train$datetime)
+  train$hday <- hour(train$datetime)
+  train <- train[, !names(train) %in% c("datetime")]
+  
+  # also drop categorical 
+  #train <- train[, !names(train) %in% c("holiday", "workingday", "dmon")]
+  
+  # create dummy variables for categorical variables
+  train <- predict(dummyVars( ~ ., data=train), train)
+  train <- as.data.frame(train)
+  
+  # variable registered is highly correlated with variable count
+  # weather.4 has too few data points
+  train <- train[, !names(train) %in% c("registered", "weather.4")]
+  
+  return(train)
+}
+
+# my very first try, :)
+m <- count ~ . + I(hday^2) + I(hday^3) + I(temp^2) + I(humidity^2) + I(windspeed^2) +
+  hday:season.1 + hday:season.2 + hday:season.3 + hday:season.4 +
+  hday:weather.1 + hday:weather.2 + hday:weather.3 +
+  temp:season.1 + temp:season.2 + temp:season.3 + temp:season.4 +
+  temp:weather.1 + temp:weather.2 + temp:weather.3 +
+  humidity:season.1 + humidity:season.2 + humidity:season.3 + humidity:season.4 +
+  humidity:weather.1 + humidity:weather.2 + humidity:weather.3 +
+  windspeed:season.1 + windspeed:season.2 + windspeed:season.3 + windspeed:season.4 +
+  windspeed:weather.1 + windspeed:weather.2 + windspeed:weather.3 +
+  casual:season.1 + casual:season.2 + casual:season.3 + casual:season.4 +
+  casual:weather.1 + casual:weather.2 + casual:weather.3 +
+  casual:hday + casual:temp + casual:humidity + casual:windspeed
+
+
+cvControl <- trainControl(method = "cv",
+                          number = 10)
+
+
+trainGbm <- function(train.df) {
+  registerDoMC(cores=4)
+  
+  # stochastic boosted machine
+  gbmGrid <-  expand.grid(interaction.depth = seq(1, 15),
+                          n.trees = (1:100) * 10,
+                          shrinkage = c(0.1, 0.5, 1.0))
+  
+  gbmTune <- train(m, data=train.df, method="gbm",
+                   trControl=cvControl,
+                   preProc=c("center", "scale"),
+                   tuneGrid=gbmGrid)
+  saveRDS(gbmTune, "gbmTune.R")
+  return(gbmTune)
+  # best result from previous run
+  # shrinkage interaction.depth n.trees      RMSE  Rsquared   RMSESD RsquaredSD
+  # 0.1                 6     330  98.64953 0.7034381 2.320874 0.01441372
+  
+}
+
+trainKNN <- function(train.df) {
+  registerDoMC(cores=4)
+  # KNN
+  knnTune <- train(m, data=train.df, method="knn",
+                   preProc=c("center", "scale"),
+                   tuneGrid=data.frame(.k=1:30),
+                   trControl=cvControl)
+  # k   RMSE  Rsquared  RMSE SD  Rsquared SD
+  # 7  112   0.619     4.65     0.0164
+  saveRDS(knnTune, "knnTune")
+  return(knnTune)
+}
+  
+
+trainNnet <- function(train.df) {
+  registerDoMC(cores=4)
+# Neural network
+  nnetGrid <- expand.grid(.decay=c(0, 0.01, 0.1), .size=c(1:15), .bag=F)
+  nnetTune <- train(m, data=train.df, method="avNNet", tuneGrid=nnetGrid,
+                    preProc=c("center", "scale"),
+                    linout=F,
+                    trace=F,
+                    MaxNWts=10000,
+                    maxit=500)
+  
+  saveRDS(nnetTune, "nnetTune")
+  return(nnetTune)
+}
+
+
+trainMARS <- function(train.df) {
+  registerDoMC(cores=1)
+  # MARS
+  marsGrid <- expand.grid(.degree = 1:5, .nprune = 2:100)
+  marsTune <- train(m, data=train.df, method="earth", tuneGrid=marsGrid,
+                    preProc=c("center", "scale"))
+
+  saveRDS(marsTune, "marsTune")
+  return(marsTune)
+}
+
+
+#trainSVM <- function () {
+# SVM RBF
+#svmRTune <- train(m, data=train, method="svmPoly",
+#                  preProc=c("center", "scale"),
+#                  tuneLength=25,
+#                  trControl=cvControl)
+#}
+
+# run all
+train.df <- preprocess.data()
+gbmTune <- trainGbm(train.df)
+knnTune <- trainKNN(train.df)
+nnetTune <- trainNnet(train.df)
+marsTune <- trainMARS(train.df)
